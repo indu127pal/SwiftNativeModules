87f734390c2a625d1eb75b98a06549b3bcd65487
indu127pal <ipal127@gmail.com>
2019-01-29 16:08:03 +0530

    add first version

---
+++ ios/GeoModule.m
@@ -0,0 +1,15 @@
+//
+//  GeoModule.m
+//  SwiftModuleApp
+//
+//  Created by Indu Pal on 11/11/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+// GeoModule.m
+
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(GeofenceModule, NSObject)
+
+@end
